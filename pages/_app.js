d49435b500e232be7d0733fd25649cc3d2396024
AshishSharma03 <30octaksharma@gmail.com>
2023-09-22 23:05:00 +0530

    first-update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import ThemeRegistry from '../muiconfigs/ThemeRegistry'
 import '../styles/globals.css'
+import { StoreProvider } from '../utils/store'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeRegistry>
+  <StoreProvider>
+
+  <Component {...pageProps} />
+  </StoreProvider>
+  </ThemeRegistry>
+  )
 }
 
 export default MyApp
